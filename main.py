ac1a7c48485468a03349b0fe78de9103c07175a3
tp-coder <onikaze-polatti@hotmail.com>
2024-12-11 18:19:50 +0100

    Working on count characters feature

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
 def menu():
     print("What you would you like to do?")
-    print("Choose 'Read' to read the book and 'count' to know how many words the book has.")
-    option = input("Enter your choice: read or count? ").strip().lower()
+    print("Choose 1 to read the book, 2 to know how many words the book has, and 3 to count the characters.")
+    option = input("Enter your choice: 1, 2 or 3? ")
     print("Got it!")
     
-    if option == "read":
-        read_the_book()
+    if option == "1":
+        read_book()
         exit()
-    elif option == "count":
-        count_the_words()
+    elif option == "2":
+        count_words()
+        exit()
+    elif option == "3":
+        count_characters()
         exit()
     else:
         print("Invalid choice. Please try again.")
 
-def count_the_words():
+def read_book():
     try:
         with open("./books/Frankenstein.txt") as f:
-            file_contents = f.read()
-            word_count = len(file_contents.split())
-            print(f"The book has {word_count} words")
+            read_book = f.read()
+            print(read_book)
+
     except FileNotFoundError:
         print("Path to the book not found.")
 
-def read_the_book():
+def count_words():
     try:
         with open("./books/Frankenstein.txt") as f:
-            file_contents = f.read()
-            print(file_contents)
+            count_words = len(f.read().split())
+            print(f"The book has {count_words} words")
+
     except FileNotFoundError:
+        print("Path to the book not found.")
+
+def count_characters():
+    try:
+        with open("./books/Frankenstein.txt") as f:
+            book = f.read().strip()
+            count_characters = 0
+
+            for letter in book:
+                count_characters += 1
+
+            print(count_characters)                
+
+    except FileExistsError:
         print("Path to the book not found.")
 
 def main():
